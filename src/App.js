29069ea52b7baceee716f28ad0462b3be312012a
Amogh Nandodkar <nandodkaramogh@gmail.com>
2022-01-26 14:24:45 +0530

    Cart Buttons

--- src/App.js
+++ src/App.js
@@ -23,9 +23,27 @@ const App = () => {
   }
 
   const handleAddToCart = async(productId, quantity) => {
-    const item = await commerce.cart.add(productId, quantity);
+    const { cart } = await commerce.cart.add(productId, quantity);
 
-    setCart(item.cart);
+    setCart(cart);
+  }
+
+  const handleUpdateCartQty = async (productId, quantity) => {
+    const {cart } = await commerce.cart.update(productId, { quantity });
+
+    setCart(cart);
+  }
+
+  const handleRemoveFromCart = async(productId) => {
+    const { cart } = await commerce.cart.remove(productId);
+    
+    setCart(cart)
+  };
+
+  const handleEmptyCart = async () => {
+    const { cart } = await commerce.cart.empty();
+
+    setCart(cart)
   }
 
   useEffect(() => {
@@ -44,7 +62,12 @@ const App = () => {
           <Route exact path='/' element={<Products products = {products} onAddToCart = {handleAddToCart}/>}>
             
           </Route>
-          <Route exact path='/cart' element={<Cart cart={cart} />}>
+          <Route exact path='/cart' element={<Cart 
+            cart={cart} 
+            handleRemoveFromCart = {handleRemoveFromCart}
+            handleUpdateCartQty = {handleUpdateCartQty}
+            handleEmptyCart = {handleEmptyCart}
+          />}>
             
           </Route>
         </Routes>
